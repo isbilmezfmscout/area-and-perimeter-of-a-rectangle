bcde5862cd6c5e6c55f4a802f7576cf770694860
Mehmet Emin Kaya <99548760+isbilmezfmscout@users.noreply.github.com>
2022-02-16 23:52:28 +0300

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 23:43:01 2022
+
+@author: mehme
+"""
+
+a = int(input("Write First Side of the Rectangle:"))
+b = int(input("Write Second Side of the Rectangle:"))
+c = a*b
+d = a + b
+
+print("Area of Rectangle",c)
+print("Perimeter of Rectangle",2*d)
